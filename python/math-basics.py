5acb4c1456c2bc29ac9238b71e8229ba9aa258c2
Lean Okrie <Leanokrie@proton.me>
2026-02-25 13:35:12 +0000

    Initial commit

---
+++ python/math-basics.py
@@ -0,0 +1,20 @@
+import math
+
+# first_cathetus = float(input("Enter a number: "))
+# second_cathetus = float(input("Enter a second number: "))
+# result = math.sqrt(pow(first_cathetus, 2) + pow(second_cathetus, 2))
+# print(f"the result is: {round(result, 2)}")
+
+# radius = float(input("Enter the radius of the circle: "))
+# area = math.pi * pow(radius, 2)
+# result = round(area, 2)
+# print(f"The area of the circle is: {result}")
+
+# side1 = float(input("Enter the length of the first side of the triangle: "))
+# side2 = float(input("Enter the length of the second side of the triangle: "))
+# perimeter = side1 + side2 + math.hypot(side1, side2)
+# print(f"The perimeter of the triangle is: {round(perimeter, 2)}")
+
+# radius = float(input("Enter the radius of the sphere: "))
+# volume = (4/3) * math.pi * pow(radius, 3)
+# print(f"The volume of the sphere is: {round(volume, 2)}")
